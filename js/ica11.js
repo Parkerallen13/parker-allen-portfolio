2571ed42653e8998a04107855463758f9e60dc14
Parkerallen13 <parkerjeanneallen@gmail.com>
2024-11-06 23:15:57 -0700

    corrected js file

--- js/ica11.js
+++ js/ica11.js
@@ -1,19 +1,51 @@
 function tellFortune(numChildren, partnerName, location, jobTitle) {
     let fortune = `You will be a ${jobTitle} in ${location}, and married to ${partnerName} with ${numChildren} kids.`;
-    document.getElementById('fortune').innerHTML += `<p>${fortune}</p>`;
-}
+    document.getElementById("fortune").innerHTML += `<p>${fortune}</p>`;
+  }
 
-console.log(tellFortune(3, 'Alex', 'New York', 'Software Developer'));
-console.log(tellFortune(1, 'Jordan', 'Paris', 'Artist'));
-console.log(tellFortune(2, 'Taylor', 'Tokyo', 'Teacher'));
-
-
-function calculateDogAge(puppyAge) {
+  function calculateDogAge(puppyAge) {
     let dogAge = puppyAge * 7;
     let result = `Your doggie is ${dogAge} years old in dog years!`;
-    document.getElementById('dog-age').innerHTML += `<p>${result}</p>`;
-}
+    document.getElementById("dogAge").innerHTML += `<p>${result}</p>`;
+  }
 
-calculateDogAge(1);
-calculateDogAge(3);
-calculateDogAge(5);
+  function reverseNumber(number) {
+    let numString = number.toString();
+    let newNumber = [];
+
+    for (let i = numString.length - 1; i >= 0; i--) {
+      newNumber += numString[i];
+    }
+    document.getElementById(
+      "reverseNumber"
+    ).innerHTML += `<p>${newNumber}</p>`;
+  }
+
+  function alphabet(word) {
+    let newWordArray = [];
+
+    for (let i = 0; i < word.length; i++) {
+      newWordArray.push(word[i]);
+    }
+    newWordArray.sort();
+    let newWord = "";
+    for (let i = 0; i < newWordArray.length; i++) {
+      newWord += newWordArray[i];
+    }
+    document.getElementById(
+      "alphabetOrder"
+    ).innerHTML += `<p>${newWord}</p>`;
+  }
+
+  function titleCase(sentence) {
+    let result = "";
+
+    for (let i = 0; i < sentence.length; i++) {
+      if (i === 0 || sentence[i - 1] === " ") {
+        result += sentence[i].toUpperCase();
+      } else {
+        result += sentence[i];
+      }
+    }
+    document.getElementById("titleCase").innerHTML += `<p>${result}</p>`;
+  }
