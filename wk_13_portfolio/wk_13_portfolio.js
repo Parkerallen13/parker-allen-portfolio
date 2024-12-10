d6c43f2e599a8547e0316c10052141a6f779fbbe
Parkerallen13 <parkerjeanneallen@gmail.com>
2024-12-10 14:31:29 -0700

    added and adjusted files for json portfolio

--- wk_13_portfolio/wk_13_portfolio.js
+++ wk_13_portfolio/wk_13_portfolio.js
@@ -1,1 +1,29 @@
-console.log("hello");
+fetch('../wk_13_portfolio/wk_13_portfolio.json')
+    .then(response => response.json())
+    .then(projects => {
+        console.log(projects);
+        parseData(projects);
+    })
+    .catch(err => {
+        console.error(`Error: ${err}`);
+    });
+
+function parseData(data) {
+    const projectsContainer = document.getElementById("projects");
+
+    data.projects.forEach(project => {
+        const projectHTML = `
+            <div class="row project" id="${project.subdomain}">
+                <div class="projimg">
+                    <img src="${project.mainimg[0]}" alt="${project.mainimg[1]}">
+                </div>
+                <div class="description">
+                    <h2>${project.name}</h2>
+                    <p class="subtitle">${project.description}</p>
+                </div>
+            </div>
+        `;
+
+        projectsContainer.innerHTML += projectHTML;
+    });
+}
